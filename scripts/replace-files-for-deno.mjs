e7d7f9624bfcb4e461bca9239b001604669afd1e
suhaotian <h4av.one@gmail.com>
2024-11-15 14:20:21 +1100

    Fix

--- scripts/replace-files-for-deno.mjs
+++ scripts/replace-files-for-deno.mjs
@@ -4,6 +4,7 @@ import fs from 'fs/promises';
 async function start() {
   const files = await glob(['./src/*.ts']);
   const pluginFiles = await glob(['./src/plugins/*.ts']);
+  const nestedPluginFiles = await glob(['./src/plugins/**/*.ts']);
 
   await Promise.all(
     files.map(async (item) => {
@@ -20,6 +21,18 @@ async function start() {
     pluginFiles.map(async (item) => {
       let content = await fs.readFile(item, 'utf8');
       content = content.replace(/'xior\/utils'/g, "'../utils'");
+      content = content.replace(/from '\.(.*)';/g, (a, b, c) => {
+        return "from '." + b + ".ts';";
+      });
+      return fs.writeFile(item, content);
+    })
+  );
+
+  await Promise.all(
+    nestedPluginFiles.map(async (item) => {
+      if (pluginFiles.includes(item)) return;
+      let content = await fs.readFile(item, 'utf8');
+      content = content.replace(/'xior\/utils'/g, "'../../utils'");
       content = content.replace(/from '\.(.*)';/g, (a, b, c) => {
         return "from '." + b + ".ts';";
       });
