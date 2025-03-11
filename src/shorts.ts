bb9f7836ead30c22d41ee91ad46ae12f4c9d6172
suhaotian <h4av.one@gmail.com>
2025-03-11 12:07:48 +1100

    Add ts comment to shorts naming

--- src/shorts.ts
+++ src/shorts.ts
@@ -1,22 +1,43 @@
+/** "object" */
 export const o = 'object';
+/** "function" */
 export const f = 'function';
+/** "prototype" */
 export const p = 'prototype';
+/** "text" */
 export const text = 'text';
+/** "json" */
 export const json = 'json';
+/** "abort" */
 export const abort = 'abort';
+/** "paramsSerializer" */
 export const qs = 'paramsSerializer';
+/** "addEventListener" */
 export const addEventListener = 'addEventListener';
+/** "status" */
 export const status = 'status';
+/** Object */
 export const O = Object;
+/** undefined */
 export const undefinedValue = undefined;
+/** Object.prototype */
 export const op = O[p];
+/** Object.keys */
 export const keys = O.keys;
+/** Array.isArray */
 export const isArray = Array.isArray;
 
+/** "HEAD" */
 export const HEAD = 'HEAD';
+/** "GET" */
 export const GET = 'GET';
+/** "OPTIONS" */
 export const OPTIONS = 'OPTIONS';
+/** "POST" */
 export const POST = 'POST';
+/** "PUT" */
 export const PUT = 'PUT';
+/** "PATCH" */
 export const PATCH = 'PATCH';
+/** "DELETE" */
 export const DELETE = 'DELETE';
