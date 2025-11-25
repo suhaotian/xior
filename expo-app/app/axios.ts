91f4dd83df25383d64fd63c47b7f605257215a8e
suhaotian <h4av.one@gmail.com>
2025-11-25 15:58:14 +1100

    Add expo-app example with build

---
+++ expo-app/app/axios.ts
@@ -0,0 +1,5 @@
+import axios, { AxiosResponse, AxiosError, AxiosRequestConfig } from 'xior';
+
+const http = axios.create({});
+
+export { http };
---
+++ expo-app/app/modal.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+import { http } from './axios';
+import { useEffect } from 'react';
+export default function ModalScreen() {
+  useEffect(() => {
+    http({ url: 'https://google/coom' });
+  }, []);
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">This is a modal</ThemedText>
+      <Link href="/" dismissTo style={styles.link}>
+        <ThemedText type="link">Go to home screen</ThemedText>
+      </Link>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
